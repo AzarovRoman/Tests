d48716001458095292d41a6864dc0de7e8105563
AzarovRom <azarovrom9215@gmail.com>
2024-06-29 17:21:14 +0300

    added layers

---
+++ Tests.DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tests.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        // db tables
+        // public DbSet<XXX> XXXs { get; set; }
+    }
+}
