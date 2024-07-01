60cc7b2c5a01f4a48b86290aa3e19163bf60d2fb
Roman Azarov <azarovrom9215@gmail.com>
2024-07-01 23:52:36 +0300

    fix: добавил комментарии

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,17 +1,18 @@
 using Tests.BLL.MapperProfiles;
 using Tests.Extensions;
-using Tests.DAL.Repositories;
-using Tests.DAL.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
-builder.Services.RegisterDbContext(configuration);
-builder.Services.RegisterProjectRepositories();
-builder.Services.RegisterProjectServices();
-builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
 
+// Добавляем контекст базы данных в Di-контейнер
+builder.Services.RegisterDbContext(configuration);
+// Добавляем репозитории (DAL) в Di-контейнер
+builder.Services.RegisterProjectRepositories();
+// Добавляем сервисы (BLL) в Di-контейнер
+builder.Services.RegisterProjectServices();
+// Добавляем маппер (BLL) в Di-контейнер
 builder.Services.AddAutoMapper(typeof(QuestionsMapper).Assembly);
 
 builder.Services.AddControllers();
