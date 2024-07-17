8c20f0eff87162f60f356430fa8dfdf0ff1fd1e6
Roman Azarov <azarovrom9215@gmail.com>
2024-07-18 01:04:29 +0300

    feet: добавил связь теста и вопроса

--- Tests.DAL/Entities/Question.cs
+++ Tests.DAL/Entities/Question.cs
@@ -8,5 +8,6 @@ namespace Tests.DAL.Entities
         public DifficultyEnum Difficulty { get; set; }
         public string Text { get; set; }
         public ICollection<Answer> Answers { get; set; }
+        public ICollection<Test> Tests { get; set; }
     }
 }
