9435f1c36e021c74a223a590e7474bf3d1f3e7af
FaggarHero@outlook.com <FaggarHero@outlook.com>
2024-08-06 16:38:26 +0300

    добавила описания к полям и методам, юобавила сущность тега и методы добавления, получения и удаления

--- Tests.BLL/Models/AnswerModel.cs
+++ Tests.BLL/Models/AnswerModel.cs
@@ -3,7 +3,13 @@
     public class AnswerModel
     {
         public int Id { get; set; }
+        /// <summary>
+        /// Текст ответа
+        /// </summary>
         public string Text { get; set; }
+        /// <summary>
+        /// Обозначение правильности ответа
+        /// </summary>
         public bool IsRight { get; set; }
     }
 }
--- Tests.BLL/Models/TestModel.cs
+++ Tests.BLL/Models/TestModel.cs
@@ -9,7 +9,13 @@ namespace Tests.BLL.Models
     public class TestModel
     {
         public int Id { get; set; }
+        /// <summary>
+        /// Список вопросов теста
+        /// </summary>
         public List<QuestionModel> Questions { get; set; }
+        /// <summary>
+        /// Название теста
+        /// </summary>
         public string Name { get; set; }
 
     }
